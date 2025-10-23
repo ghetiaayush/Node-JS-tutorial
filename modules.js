8f995a423d6983f0d37314adf8b733e77a08d377
ayushghetia <55269750+ghetiaayush@users.noreply.github.com>
2025-10-23 11:54:26 +0530

    modules refrence

--- modules.js
+++ modules.js
@@ -1,6 +1,16 @@
+//Core Modules
 const fs = require("fs");
 fs.writeFileSync("dummy.txt", "trying with modules"); 
 
 const os = require("os"); // import os module
 console.log(os.platform()); // Print the platform of the OS
 console.log(os.hostname()); // Print the hostname of the OS
+//console.log(os.cpus()); // Print the CPU information of the OS
+
+// Global Object
+console.log(process.cwd());
+console.log(process.pid);
+
+//custom global object
+const{log} = require("console");
+log("This is a message using destructured console log");
