612d1da9f57f606ff94a20c57fe6dc26e2122ed3
ayushghetia <55269750+ghetiaayush@users.noreply.github.com>
2025-10-23 13:33:59 +0530

    multiple server example and nodemon package globaly

--- server.js
+++ server.js
@@ -5,5 +5,11 @@ const http = require("http");
 // Understanding Request and Response Parameters 
 http.createServer((req, resp) => {
 resp.write("<h1>Hello, welcome to nodejs tutorial</h1>");
-resp.end("Hello"); // Ends the response
+resp.end("Hello from step by step coding"); // Ends the response
 }).listen(4800);
+
+//multiple server example
+http.createServer((req, resp) => {
+resp.write("<h2>Hello, welcome to nodejs tutorial</h2>");
+resp.end("Hello"); // Ends the response
+}).listen(8000);
