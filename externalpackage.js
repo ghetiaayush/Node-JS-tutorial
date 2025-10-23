af2dcdb87dec29b6145899d48151984e918931c8
ayushghetia <55269750+ghetiaayush@users.noreply.github.com>
2025-10-23 12:56:34 +0530

    initial commit

---
+++ externalpackage.js
@@ -0,0 +1,6 @@
+// Example of Using an External Package
+const colors = require("colors");
+
+console.log(colors.red("welcome to nodejs tutorials"));
+console.log(colors.green("welcome to nodejs tutorials"))
+console.log(colors.yellow("welcome to nodejs tutorials"))
