c20eaf4420810f6e690a14ca4bfb4a88f2928bba
ayushghetia <55269750+ghetiaayush@users.noreply.github.com>
2025-11-23 22:01:14 +0530

    Submit Form with Node.js method 1

---
+++ form.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+http.createServer(
+   (req, res) => {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      if (req.url === '/') {
+         res.write(`
+         <form method="POST" action="/submit">
+         <input type="text" name="email" placeholder="enter email"/>
+         <input type="text" name="password" placeholder="enter password"/>
+         <button type="submit">Submit</button>
+         </form>`);
+      res.end();
+      }
+      else if (req.url === '/submit') {
+         res.write('Form submitted successfully!');
+         res.end();
+      }
+   }
+).listen(3200);
