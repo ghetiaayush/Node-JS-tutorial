6872a1f26d1ecde0d8bdba53a91ab83575294052
ayushghetia <55269750+ghetiaayush@users.noreply.github.com>
2025-12-07 21:38:34 +0530

    Modules adn Routes | custom modules in node js

--- userDataSubmit.js
+++ userDataSubmit.js
@@ -1,7 +1,28 @@
-function userDataSubmit(req,res) {
-   
-   res.write(`<h1>Form Submitted</h1>`);
+// function userDataSubmit(req,res) {res.write(`<h1>Form Submitted</h1>`);}module.exports = userDataSubmit;
 
+const querystring = require("querystring");
+
+function userDataSubmit(req, resp) {
+  let dataBody = [];
+
+  req.on("data", (chunk) => {
+    dataBody.push(chunk);
+  });
+
+  req.on("end", () => {
+    let rawData = Buffer.concat(dataBody).toString();
+    let readableData = querystring.parse(rawData);
+
+    let dataString =
+      "My name is " +
+      readableData.name +
+      " and my password is " +
+      readableData.password;
+
+    console.log(dataString);
+  });
+
+  resp.write(`<h1>You can get data from user form here</h1>`);
 }
 
 module.exports = userDataSubmit;
